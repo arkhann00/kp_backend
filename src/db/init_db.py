30f089ff0c938a936d0df590e7beeb2c04c3448d
arkhann00 <khaarsyan@gmail.com>
2026-01-27 16:38:13 +0300

    Поменял инит базы

--- src/db/init_db.py
+++ src/db/init_db.py
@@ -6,5 +6,5 @@ from src.models.user import User
 
 async def init_db():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+        # await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
